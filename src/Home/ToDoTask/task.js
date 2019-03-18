86bc0f4694c488be90b6db4a81ce66f07a3f62b4
hadeer-attalluh <hadeermohamed.fcis@gmail.com>
2019-03-18 10:34:53 +0200

    edit styling

--- src/Home/ToDoTask/task.js
+++ src/Home/ToDoTask/task.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Form,ListGroup } from 'react-bootstrap';
+import { Form, ListGroup, Col, Row } from 'react-bootstrap';
 
-import {connect} from 'react-redux';
-import {CompleteToDoAction} from '../../Store/Actions/todo';
-import {UncompleteToDoAction} from '../../Store/Actions/todo';
-import {DeleteToDoAction} from '../../Store/Actions/todo';
-import {unDeleteToDoAction} from '../../Store/Actions/todo';
+import { connect } from 'react-redux';
+import { CompleteToDoAction } from '../../Store/Actions/todo';
+import { UncompleteToDoAction } from '../../Store/Actions/todo';
+import { DeleteToDoAction } from '../../Store/Actions/todo';
+import { unDeleteToDoAction } from '../../Store/Actions/todo';
 
 export class ToDoTask extends React.Component {
     constructor(props) {
@@ -17,27 +17,22 @@ export class ToDoTask extends React.Component {
         this.onDelete = this.onDelete.bind(this);
     }
 
-    onChange(e) {      
+    onChange(e) {
         console.log(this.props)
-            if(e.target.checked)
-            {
-                this.props.done(this.props.id);
-            }
-            else
-            {
-                this.props.undone(this.props.id);
-            }
+        if (e.target.checked) {
+            this.props.done(this.props.id);
+        }
+        else {
+            this.props.undone(this.props.id);
+        }
     }
-    onDelete()
-    {
+    onDelete() {
         console.log(this.props)
-        if(!this.props.deleted)
-        {
+        if (!this.props.deleted) {
             //toggle
             this.props.delete(this.props.id);
         }
-        else
-        {
+        else {
             debugger;
             this.props.undelete(this.props.id);
         }
@@ -46,16 +41,23 @@ export class ToDoTask extends React.Component {
         debugger
         return (
             <ListGroup.Item key={this.props.id} className="d-flex justify-content-between">
-                {this.props.task}
-
-                <Form.Check
-                    label="Done"
-                    name="complete"
-                    onChange={this.onChange}
-                    checked={this.props.complete}
-                    disabled={this.props.deleted}
-                />
-                <i className="fas fa-trash-alt" onClick={this.onDelete}>{(this.props.deleted?"Undelete":"Delete")}</i>
+                <Col col={10}>
+                    {this.props.task}
+                </Col>
+                <Row>
+                    <Col col={1}>
+                        <Form.Check
+                            label="Done"
+                            name="complete"
+                            onChange={this.onChange}
+                            checked={this.props.complete}
+                            disabled={this.props.deleted}
+                        />
+                    </Col>
+                    <Col col={1}>
+                        <i className="fas fa-trash-alt" onClick={this.onDelete}>{(this.props.deleted ? "Undelete" : "Delete")}</i>
+                    </Col>
+                </Row>
             </ListGroup.Item>
         );
     }
@@ -64,10 +66,10 @@ export class ToDoTask extends React.Component {
 const mapDispatchToProps = dispatch => (
     {
         done: id => dispatch(CompleteToDoAction(id)),
-        undone: id=> dispatch(UncompleteToDoAction(id)),
-        delete: id=> dispatch(DeleteToDoAction(id)),
-        undelete: id=> dispatch(unDeleteToDoAction(id)),
+        undone: id => dispatch(UncompleteToDoAction(id)),
+        delete: id => dispatch(DeleteToDoAction(id)),
+        undelete: id => dispatch(unDeleteToDoAction(id)),
     }
 )
 
-export default connect(null,mapDispatchToProps)(ToDoTask);
+export default connect(null, mapDispatchToProps)(ToDoTask);
